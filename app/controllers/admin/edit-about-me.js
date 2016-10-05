2b48e2a96e808660f0702b80569fba4008186da2
Dominic Ritchey <dmr5bq@virginia.edu>
2016-10-05 15:32:44 -0400

    Minor changes

--- app/controllers/admin/edit-about-me.js
+++ app/controllers/admin/edit-about-me.js
@@ -2,13 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  model() {
+    return this.store.findAll('aboutdata');
+  },
+
   actions: {
-    submitAbout() {
+    submitAbout(current) {
       // ensure that the description is only ever going to be looking at the last available record
 
       const description = this.get('description');
 
       const newAbout = this.store.createRecord('aboutdata', {description: description});
+
+      current.destroyRecord();
 
       newAbout.save().then(
         console.log('Successfully updated about me.')
